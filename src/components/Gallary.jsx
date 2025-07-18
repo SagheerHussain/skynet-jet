a948b7b52ea9bd2173313cad83b15879a81f8197
Muhammad Shayan <shayanarif666@gmail.com>
2025-07-18 17:22:54 +0500

    Frontend Complete

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination } from "swiper/modules";
+import { EffectCoverflow, Pagination, Thumbs, FreeMode } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+import "swiper/css/free-mode";
+import "swiper/css/thumbs";
 
 import GallaryOne from "/images/gallary/image 1.png";
 import GallaryTwo from "/images/gallary/image 2.png";
@@ -13,7 +15,6 @@ import GallaryFour from "/images/gallary/image 4.png";
 import GallaryFive from "/images/gallary/image 5.png";
 import GallarySix from "/images/gallary/image 6.png";
 
-// Required for screen readers
 Modal.setAppElement("#root");
 
 const videoList = [
@@ -21,109 +22,69 @@ const videoList = [
     id: 1,
     title: "Climb to FL340 in 14 Mins!",
     thumbnail: GallaryOne,
-    videoUrl:
-      "https://www.youtube.com/watch?v=vQchZx9MvJ8&list=RDvQchZx9MvJ8&start_radio=1",
+    videoUrl: "https://www.youtube.com/embed/jWF2y3qpudo",
     duration: "24:10",
   },
   {
     id: 2,
     title: "Is the 940 the Ultimate TBM?",
     thumbnail: GallaryTwo,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_2",
+    videoUrl: "https://www.youtube.com/embed/jWF2y3qpudo",
     duration: "08:10",
   },
   {
     id: 3,
     title: "Time to Climb FL270",
     thumbnail: GallaryThree,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
+    videoUrl: "https://www.youtube.com/embed/jWF2y3qpudo",
     duration: "18:18",
   },
   {
     id: 4,
-    title: "Time to Climb FL270",
+    title: "Another Aviation Clip",
     thumbnail: GallaryFour,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
+    videoUrl: "https://www.youtube.com/embed/jWF2y3qpudo",
+    duration: "12:40",  
   },
   {
     id: 5,
-    title: "Time to Climb FL270",
+    title: "Flight Training Review",
     thumbnail: GallaryFive,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
+    videoUrl: "https://www.youtube.com/embed/jWF2y3qpudo",
+    duration: "15:30",
   },
   {
     id: 6,
-    title: "Time to Climb FL270",
+    title: "Test Flight Recap",
     thumbnail: GallarySix,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
-  },
-  {
-    id: 7,
-    title: "Time to Climb FL270",
-    thumbnail: GallaryOne,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
-  },
-  {
-    id: 8,
-    title: "Time to Climb FL270",
-    thumbnail: GallaryTwo,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
-  },
-  {
-    id: 9,
-    title: "Time to Climb FL270",
-    thumbnail: GallaryThree,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
-  },
-  {
-    id: 10,
-    title: "Time to Climb FL270",
-    thumbnail: GallaryFour,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
-  },
-  {
-    id: 11,
-    title: "Time to Climb FL270",
-    thumbnail: GallaryFive,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
-  },
-  {
-    id: 12,
-    title: "Time to Climb FL270",
-    thumbnail: GallarySix,
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID_3",
-    duration: "18:18",
+    videoUrl: "https://www.youtube.com/embed/jWF2y3qpudo",
+    duration: "11:00",
   },
 ];
 
 const Gallary = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [currentVideo, setCurrentVideo] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const openModal = (video) => {
-    setCurrentVideo(video);
-    setIsOpen(true);
+  const openModal = (index) => {
+    setCurrentIndex(index);
+    setModalOpen(true);
   };
 
   const closeModal = () => {
-    setCurrentVideo(null);
-    setIsOpen(false);
+    setModalOpen(false);
   };
+
 
   return (
     <>
       <section id="gallary" className="w-full py-20">
         <div className="container px-5">
           <div className="text-center text-white">
-            <h1 className="text-4xl font-bold">A Bespoke Approach to Brokerage</h1>
+            <h1 className="text-4xl font-bold">
+              A Bespoke Approach to Brokerage
+            </h1>
             <p className="text-base max-w-2xl mx-auto mt-4">
               Explore our curated video library showcasing real-world
               performance, cockpit experiences, and expert reviews of the
@@ -149,11 +110,11 @@ const Gallary = () => {
             modules={[EffectCoverflow]}
             className="mySwiper"
           >
-            {videoList.map((video) => (
+            {videoList.map((video,index) => (
               <SwiperSlide
                 key={video.id}
                 className="relative w-[300px]"
-                // onClick={() => openModal(video)}
+                onClick={() => openModal(index)}
               >
                 <img
                   src={video.thumbnail}
@@ -171,33 +132,58 @@ const Gallary = () => {
           </Swiper>
 
           {/* Modal */}
-          {/* <Modal
-            isOpen={modalIsOpen}
+          <Modal
+            isOpen={modalOpen}
             onRequestClose={closeModal}
-            contentLabel="Video Modal"
-            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80"
-            overlayClassName="z-[9999999] fixed inset-0 bg-black bg-opacity-80"
+            contentLabel="Video Gallery Modal"
+            className="fixed inset-0 flex items-center justify-center bg-black/90 z-[999]"
+            overlayClassName="z-[9999]"
           >
-            <div className="relative w-[90%] md:w-[60%] h-[60vh]">
+            <div className="bg-black p-4 rounded-lg w-[95%] md:w-[80%] max-w-5xl">
               <button
                 onClick={closeModal}
-                className="absolute top-0 right-0 text-white text-4xl font-bold p-2 z-[999]"
+                className="text-white text-3xl font-bold absolute top-5 right-8"
               >
                 &times;
               </button>
-              {currentVideo && (
+
+              {/* Main Video */}
+              <div className="aspect-video mb-6">
                 <iframe
-                  width="100%"
-                  height="100%"
-                  src={currentVideo.videoUrl}
-                  title="Video Player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  src={videoList[currentIndex].videoUrl}
+                  title={videoList[currentIndex].title}
                   allowFullScreen
+                  className="w-full h-full rounded"
                 ></iframe>
-              )}
+              </div>
+
+              {/* Thumbnails */}
+              <Swiper
+                onSwiper={setThumbsSwiper}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Thumbs]}
+                className="mySwiper"
+              >
+                {videoList.map((video, i) => (
+                  <SwiperSlide key={video.id} className="cursor-pointer">
+                    <img
+                      src={video.thumbnail}
+                      alt={video.title}
+                      className={`rounded w-full h-[80px] object-cover border-2 ${
+                        currentIndex === i
+                          ? "border-blue-500"
+                          : "border-transparent"
+                      }`}
+                      onClick={() => setCurrentIndex(i)}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
-          </Modal> */}
+          </Modal>
         </div>
       </section>
     </>
